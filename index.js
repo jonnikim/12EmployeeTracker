e305e03c99edf49b612034a76bcdc7018ddcd9e4
Jonathan <jonathan213kim@gmail.com>
2020-06-27 22:55:15 -0700

    Completed View Employee/Role/Department, Add Employee/Role/Department, Update Employee Role

--- index.js
+++ index.js
@@ -57,44 +57,59 @@ function mainMenu() {
     });
 }
 function viewEmployee() {
-  connection.query("SELECT * FROM employee", (err, res) => {
-    if (err) throw err;
-    for (let i = 0; i < res.length; i++) {
-      //   console.log(
-      //     "ID: " +
-      //       res[i].id +
-      //       " || First Name: " +
-      //       res[i].first_name +
-      //       " || Last Name: " +
-      //       res[i].last_name +
-      //       " || Role: " +
-      //       res[i].role_id +
-      //       " || Department: " +
-      //       res[i].department_id +
-      //       " || Salary: " +
-      //       res[i].salary +
-      //       " || Manager: " +
-      //       res[i].manager_id
-      //   );
-      console.table([
-        "ID",
-        "First Name",
-        "Last Name",
-        "Role",
-        "Department",
-        "Salary",
-        "Manager",
-      ]);
+  connection.query(
+    `SELECT e.id AS employee_id, e.first_name, e.last_name, d.name AS department_name, r.title AS job_title, r.salary, CONCAT(f.first_name, " ", f.last_name) AS manager_name FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON d.id = r.department_id LEFT JOIN employee f ON e.manager_id = f.id;`,
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
     }
-  });
+  );
 }
 function viewRole() {
+  connection.query(
+    `SELECT r.id AS role_id, r.title, r.salary, d.name AS department_name
+    FROM role r
+    LEFT JOIN department d
+    ON r.department_id = d.id;`,
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+    }
+  );
   console.log("Viewing Role");
 }
 function viewDepartment() {
+  connection.query(`SELECT * FROM department`, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+  });
   console.log("Viewing Department");
 }
 function newEmployee() {
+  let role = [];
+  connection.query(`SELECT role.title FROM role`, (err, res) => {
+    if (err) throw err;
+    for (let m = 0; m < res.length; m++) {
+      role.push(res[m].title);
+    }
+  });
+  let manager = [];
+  let manager_id = [];
+  connection.query(
+    `SELECT DISTINCT CONCAT(x.first_name, " ", x.last_name) AS manager_name, x.id AS manager_id 
+    FROM employee e
+    LEFT JOIN employee x
+    ON e.manager_id = x.id;`,
+    (err, res) => {
+      //   console.log(res);
+      for (let i = 0; i < res.length; i++) {
+        if (res[i].manager_name !== null) {
+          manager.push(res[i].manager_name);
+          manager_id.push(res[i].manager_id);
+        }
+      }
+    }
+  );
   inquirer
     .prompt([
       {
@@ -109,25 +124,28 @@ function newEmployee() {
       },
       {
         name: "role",
-        type: "input",
+        type: "list",
         message: "What is this employee's Role?",
+        choices: role,
       },
       {
-        name: "manager",
-        type: "input",
+        name: "managers",
+        type: "list",
         message: "Who is the Manager?",
+        choices: manager,
       },
     ])
     .then(function (answer) {
-      const roleValue = 2;
-      const managerValue = 2;
+      let roleIndex = role.indexOf(answer.role) + 1;
+      let managerIndex = manager.indexOf(answer.managers);
+      let managerID = manager_id[managerIndex];
       connection.query(
         "INSERT INTO employee SET ?",
         {
           first_name: answer.first_name,
           last_name: answer.last_name,
-          role_id: roleValue,
-          manager_id: managerValue,
+          role_id: roleIndex,
+          manager_id: managerID,
         },
         function (err) {
           if (err) throw err;
@@ -138,23 +156,112 @@ function newEmployee() {
     });
 }
 function newRole() {
-  console.log("Adding new Role");
+  viewRole();
+  connection.query(`SELECT * FROM department`, (err, res) => {
+    const departmentID = [];
+    const departmentName = [];
+    for (let k = 0; k < res.length; k++) {
+      departmentID.push(res[k].id);
+      departmentName.push(res[k].name);
+    }
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "role",
+          type: "input",
+          message: "What is the name of the role?",
+        },
+        {
+          name: "salary",
+          type: "number",
+          message: "What is the salary amount?",
+        },
+        {
+          name: "newDepartmentID",
+          type: "list",
+          message: "What department does it belong to?",
+          choices: departmentName,
+        },
+      ])
+      .then(function (answer) {
+        let index = departmentName.indexOf(answer.newDepartmentID);
+        connection.query(
+          "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
+          [answer.role, answer.salary, departmentID[index]],
+          function (err, res) {
+            if (err) throw err;
+          }
+        );
+        viewRole();
+      });
+  });
 }
 function newDepartment() {
-  console.log("Adding New Department");
+  inquirer
+    .prompt({
+      name: "department",
+      type: "input",
+      message: "What is the name of the department?",
+    })
+    .then(function (answer) {
+      console.log(answer);
+      let department = answer.department;
+      console.log(department);
+      connection.query(
+        "INSERT INTO department (name) VALUES (?)",
+        answer.department,
+        function (err, res) {
+          if (err) throw err;
+        }
+      );
+      viewDepartment();
+    });
 }
 function updateRole() {
-  const name = [];
+  const firstName = [];
+  const lastName = [];
+  const fullName = [];
+  const role = [];
   connection.query("SELECT first_name, last_name FROM employee", (err, res) => {
     if (err) throw err;
     for (let i = 0; i < res.length; i++) {
-      name.push(res[i].first_name + " " + res[i].last_name);
+      firstName.push(res[i].first_name);
+      lastName.push(res[i].last_name);
+      fullName.push(res[i].first_name + " " + res[i].last_name);
     }
-    inquirer.prompt({
-      name: "role",
-      type: "list",
-      message: "Which employee would you like to update?",
-      choices: name,
-    });
+  });
+
+  connection.query(`SELECT role.title FROM role`, (err, res) => {
+    if (err) throw err;
+    for (let m = 0; m < res.length; m++) {
+      role.push(res[m].title);
+    }
+    inquirer
+      .prompt([
+        {
+          name: "role",
+          type: "list",
+          message: "Which employee would you like to update?",
+          choices: fullName,
+        },
+        {
+          name: "newRole",
+          type: "list",
+          message: "Select new Role",
+          choices: role,
+        },
+      ])
+      .then(function (answer) {
+        let nameIndex = fullName.indexOf(answer.role);
+        let roleIndex = role.indexOf(answer.newRole) + 1;
+        connection.query(
+          "UPDATE employee SET role_id = ? WHERE first_name = ? AND last_name = ?",
+          [roleIndex, firstName[nameIndex], lastName[nameIndex]],
+          function (err, res) {
+            if (err) throw err;
+          }
+        );
+      });
   });
 }
--- seeds.sql
+++ seeds.sql
@@ -1,11 +1,40 @@
 USE businessDB;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, null), 
-("Jane", "Deer", 2, null);
+VALUES ("John", "Deer", 1, 4), 
+("Jane", "Doe", 2, 4),
+("Toby", "Flenderson", 9, null),
+("Charles", "Leman", 3, null),
+("Mark", "Twain", 2, 4),
+("Joe", "Fraiser", 1, 4),
+("Alex", "Martinez", 1, 4),
+("Booker", "Green", 7, 12),
+("Omar", "Wijesinghe", 2, 4),
+("Cameron", "Smith", 7, 12),
+("Mary", "Lee", 7, 12),
+("Suki", "Yamamoto", 8, null),
+("Trent", "Delier", 6, 16),
+("Harry", "Potter", 6, 16),
+("Kermit", "Frog", 5, 17),
+("Amy", "Williams", 4, null),
+("David", "Varela", 4, null),
+("Casey", "Jin", 5, 17);
+
+
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 50,000.00, 1);
+VALUES ("Accountant", 50000.00, 1),               
+("Senior Accountant", 80000.00, 1),              
+("Chief Finance Officer", 150000.00, 1),          
+("Brand Manager", 62000.00, 2),                  
+("Social Media Analyst", 40000.00, 2),           
+("Marketing Associate", 45000.00, 2),             
+("Legal Analyst", 95000.00, 3),                     
+("Chief Legal Officer", 130000.00, 3),             
+("Human Resources", 75000.00, 4);              
 
 INSERT INTO department (name)
-VALUES ("Finance & Accounting");
+VALUES ("Finance & Accounting"),
+("Marketing"),
+("Legal"),
+("Human Resources");
